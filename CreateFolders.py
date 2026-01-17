f3142cb90d6009069e3bed0bf6ae2cbfd5ca414f
AlexandrosMo <alexandros.moraitis99@gmail.com>
2026-01-17 18:02:14 +0200

    python Script

---
+++ CreateFolders.py
@@ -0,0 +1,14 @@
+import os
+ 
+
+def CreateFolders():
+        create_folder =('January,February,March,April,May,June,July,August,September,October,November,December')
+        
+        for month in create_folder.split(','):
+            if not os.path.exists(f"Flask/{month}"):
+                os.makedirs(f"Flask/{month}")
+
+            letter =('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z')
+            for letter in letter.split(','):
+                os.makedirs(f"Flask/{month}/{letter}")
+CreateFolders()
