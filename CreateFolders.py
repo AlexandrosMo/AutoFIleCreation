3ec3cf182b539831a1a9486b752b03a606f93da7
AlexandrosMo <alexandros.moraitis99@gmail.com>
2026-01-17 19:51:54 +0200

    Fixed py

--- CreateFolders.py
+++ CreateFolders.py
@@ -5,10 +5,10 @@ def CreateFolders():
         create_folder =('January,February,March,April,May,June,July,August,September,October,November,December')
         
         for month in create_folder.split(','):
-            if not os.path.exists(f"Flask/{month}"):
-                os.makedirs(f"Flask/{month}")
+            if not os.path.exists(f"CreateFiles/{month}"):
+                os.makedirs(f"CreateFiles/{month}")
 
             letter =('A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z')
             for letter in letter.split(','):
-                os.makedirs(f"Flask/{month}/{letter}")
+                os.makedirs(f"CreateFiles/{month}/{letter}")
 CreateFolders()
